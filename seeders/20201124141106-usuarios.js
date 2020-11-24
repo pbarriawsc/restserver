823c13513fb6ab4b80545ae3cd5510c1cc2ea050
Pablo Barría Reyes <pablo.barria@wscargo.cl>
2020-11-24 11:42:38 -0300

    implement seeders migrations sequelize

---
+++ seeders/20201124141106-usuarios.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('usuario', [{
+       nombre: 'Pablo Barria',
+       password: '123456',
+       createdAt:'2020-11-24 00:00:00',
+       updatedAt:'2020-11-24 00:00:00'
+    },
+    {
+      nombre: 'Eduardo Vidal',
+      password: '123456',
+      createdAt:'2020-11-24 00:00:00',
+      updatedAt:'2020-11-24 00:00:00'
+   }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('usuario', null, {});
+  }
+};
--- server/server.js
+++ server/server.js
@@ -3,12 +3,23 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const { request } = require('express')
+const Sequelize=require('sequelize')
+var sqlz = new Sequelize('postgres://postgres:wsc2020@localhost:5432/wscargo');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
 app.use(bodyParser.json())
 app.use(require('./routes/index'))
+
+sqlz
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
  
 app.get('/', function (req, res) {
   res.json('Bienvenido al server express')
