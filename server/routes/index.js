397b1915d8de3580d46e4c3182df723a4da69ebf
Eduardo Vidal <eduardo.vidal@wscargo.cl>
2020-12-01 14:50:04 -0300

    merge

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.use(require('./menu'))
 app.use(require('./usuario'))
 app.use(require('./login'))
 app.use(require('./tipoDireccion'))
+app.use(require('./pais'))
 
 module.exports=app;
