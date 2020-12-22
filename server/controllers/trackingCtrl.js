000ab2a82278d4b9e963c06ad96606caa58fbaf5
Pablo Barría Reyes <pablo.barria@wscargo.cl>
2020-12-21 23:34:59 -0300

    endpoint tracking get

--- server/controllers/trackingCtrl.js
+++ server/controllers/trackingCtrl.js
@@ -2,13 +2,14 @@ const client = require('../config/db.client');
 const lodash= require('lodash');
 exports.list = (req, res) => {
 	const arrayFinal=[];
-    client.query('SELECT * FROM public.tracking', "", function (err, result) {
+    client.query('SELECT T.*, c.codigo as fk_cliente_codigo,c.nombre as fk_cliente_nombre,p.codigo as fk_proveedor_codigo, p.nombre as fk_proveedor_nombre FROM public.tracking t left join public.clientes c on c.id=t.fk_cliente left join public.proveedores p on p.id=t.fk_proveedor ', "", function (err, result) {
         if (err) {
             console.log(err);
             res.status(400).send(err);
         }
         const resultHeader=result;
         const ids=[];
+        const idsClientes=[];
         if(result.rows.length>0){
         	for(var i=0;i<result.rows.length;i++){
         		ids.push(result.rows[i].id);
@@ -44,8 +45,12 @@ exports.list = (req, res) => {
 			        		}
 			        		arrayFinal.push(obj);
 			        	}
-		        		res.status(200).send(arrayFinal);
+		        		
 			        }
+
+
+
+			        res.status(200).send(arrayFinal);
 		        });
         }else{
         	res.status(200).send(arrayFinal);
@@ -107,14 +112,7 @@ exports.listByClient = (req, res) => {
   };
 
 exports.create = (req, res) => {
-	let proveedorId;
-    if (!req.body.proveedor) {
-      res.status(400).send({
-        message: "El proveedor es obligatorio",
-        success:false
-      });
-      return;
-    }else if (!req.body.fecha_creacion){
+    if (!req.body.fecha_creacion){
         res.status(400).send({
             message: "La fecha de generacion es obligatorio",
             success:false
@@ -207,6 +205,44 @@ exports.create = (req, res) => {
 			        }
 			    });
     	}
+    }else{
+    	var textQuery='INSERT INTO public.tracking(tipo,fecha_creacion,estado) VALUES($1,$2,$3) RETURNING *';
+    	var valuesQuery=[req.body.tipo,req.body.fecha_creacion,req.body.estado];
+    	if(req.body.fk_cliente){
+    		textQuery='INSERT INTO public.tracking(tipo,fecha_creacion,estado,fk_cliente) VALUES($1,$2,$3,$4) RETURNING *'
+    		valuesQuery=[req.body.tipo,req.body.fecha_creacion,req.body.estado,req.body.fk_cliente];
+    	}
+    	const query = {
+		        text: textQuery,
+		        values: valuesQuery,
+		    	};
+
+			    client.query(query,"",function (err, result) {
+			    	const err1=err;
+			        const result1=result;
+			        if (err1) {
+			            console.log(err1);
+			            res.status(400).send(err1);
+			        }
+			       
+			        if(result.rows && result.rows[0]){
+			        	if(req.body.tracking_detalle && req.body.tracking_detalle.length>0){
+			        		for(var i=0;i<req.body.tracking_detalle.length;i++){
+			        				const query2={
+						        		text: 'INSERT INTO public.tracking_detalle(fecha_recepcion,tipo_producto,producto,peso,observacion,tracking_id,estado) VALUES($1, $2,$3,$4,$5,$6,$7) RETURNING *',
+						        		values: [req.body.tracking_detalle[i].fecha_recepcion, req.body.tracking_detalle[i].tipo_producto,req.body.tracking_detalle[i].producto,req.body.tracking_detalle[i].peso,req.body.tracking_detalle[i].observacion,result.rows[0].id,req.body.tracking_detalle[i].estado],
+			        				};
+			        				client.query(query2,"",function (err, result) {
+			        					if (err) {
+					                      console.log(err);
+					                      res.status(400).send(err);
+					                    }	
+			        				});
+			        		}
+			        	}
+			        	res.status(200).send(result.rows[0]);
+			        }
+			    });
     }
     
 };
