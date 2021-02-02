68f991d7dcf49996d7a237531014179cebd566ab
Pablo Barría Reyes <pablo.barria@wscargo.cl>
2021-02-02 12:47:23 -0300

    naves_eta migration

---
+++ migrations/20210202153605-naves_eta.js
@@ -0,0 +1,78 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+     await queryInterface.createTable('naves_eta', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fk_nave: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      viaje_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      puerto_codigo:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      eta_fecha: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      eta_hora: {
+        type: Sequelize.TIME,
+        allowNull: true
+      },
+      staa_fecha: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      staa_hora: {
+        type: Sequelize.TIME,
+        allowNull: true
+      },
+      stab_fecha: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      stab_hora: {
+        type: Sequelize.TIME,
+        allowNull: true
+      },
+      etd_fecha: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      etd_hora: {
+        type: Sequelize.TIME,
+        allowNull: true
+      },
+      estado:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+     await queryInterface.dropTable('naves_eta');
+  }
+};
