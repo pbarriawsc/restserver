cb133da1364c9ab0183944771d28de6c4cfb7fa8
unknown <pablo.barria@wscargo.cl>
2021-03-08 12:13:01 -0300

    correcciones carga de contenedores

--- server/controllers/trackingCtrl.js
+++ server/controllers/trackingCtrl.js
@@ -113,7 +113,7 @@ exports.list = (req, res) => {
   exports.listByEstado = (req, res) => {
   	try{
 	const arrayFinal=[];
-    client.query('SELECT T.*, c.codigo as fk_cliente_codigo,c.nombre as fk_cliente_nombre,p.codigo as fk_proveedor_codigo, p.nombre as fk_proveedor_nombre,(SELECT count(id) FROM public.tracking_detalle WHERE tracking_id=T.id and estado=0)::integer AS bultos_pendientes,(SELECT count(id) FROM public.tracking_detalle WHERE tracking_id=T.id and estado=1)::integer AS bultos_completos FROM public.tracking t left join public.clientes c on c.id=t.fk_cliente left join public.proveedores p on p.id=t.fk_proveedor where t.estado=$1 AND t.fk_propuesta is null ORDER BY T.prioridad ASC', [req.params.estado], function (err, result) {
+    client.query('SELECT T.*, c.codigo as fk_cliente_codigo,c.nombre as fk_cliente_nombre,p.codigo as fk_proveedor_codigo, p.nombre as fk_proveedor_nombre,(SELECT count(id) FROM public.tracking_detalle WHERE tracking_id=T.id and estado=0)::integer AS bultos_pendientes,(SELECT count(id) FROM public.tracking_detalle WHERE tracking_id=T.id and estado=1)::integer AS bultos_completos FROM public.tracking t left join public.clientes c on c.id=t.fk_cliente left join public.proveedores p on p.id=t.fk_proveedor where t.estado=$1 ORDER BY T.prioridad ASC', [req.params.estado], function (err, result) {
         if (err) {
             console.log(err);
             res.status(400).send(err);
