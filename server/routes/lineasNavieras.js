fd4a011b725fa8db76b1d97834d541e751cf57b9
unknown <pablo.barria@wscargo.cl>
2021-04-19 11:59:39 -0400

    endpoint lineas navieras

--- server/routes/lineasNavieras.js
+++ server/routes/lineasNavieras.js
@@ -5,5 +5,9 @@ const prefix='/api/lineas_navieras';
 const lineasNavierasController=require('../controllers/lineasNavierasCtrl');
 
 app.get(`${prefix}`,verifyToken,lineasNavierasController.list);
+app.get(`${prefix}/estado/:estado`,verifyToken,lineasNavierasController.listByEstado);
+app.post(`${prefix}`,verifyToken,lineasNavierasController.create);
+app.put(`${prefix}/:id`,verifyToken,lineasNavierasController.update);
+app.delete(`${prefix}/:id`,verifyToken,lineasNavierasController.delete);
 
 module.exports=app;
